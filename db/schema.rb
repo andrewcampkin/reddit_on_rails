a002103965fd8fd3717848529b47cb37fdcd93eb
Andrew Campkin <hurricane666@gmail.com>
2012-09-09 21:19:26 +1200

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120909055917) do
+ActiveRecord::Schema.define(:version => 20120909090756) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "user_id"
+    t.text     "message"
+    t.integer  "link_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "links", :force => true do |t|
     t.datetime "created_at", :null => false
